b173132aa92166c37dbf40cc2a1fa1b7dcba76c1
forresti <forresti@eecs.berkeley.edu>
2016-03-30 14:23:41 -0700

    current caffe.proto from caffe-master

--- boda_env.sh
+++ boda_env.sh
@@ -1,5 +1,8 @@
-export BODA_HOME=~/git_work/boda
-export LD_LIBRARY_PATH=~/git_work/caffe_dev/build/lib
+#export BODA_HOME=~/git_work/boda
+export BODA_HOME=~/boda
+#export LD_LIBRARY_PATH=~/git_work/caffe_dev/build/lib
+export LD_LIBRARY_PATH=/nscratch/forresti/caffe-bvlc-master/build/lib:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/nscratch/forresti/caffe-bvlc-master/caffe-repro-cuDNN-NiN-bug/build/lib:$LD_LIBRARY_PATH
 export PATH=${BODA_HOME}/lib:$PATH
 . ${BODA_HOME}/boda_completion.bash
 
