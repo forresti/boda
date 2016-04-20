d0ca60b8b672b4152269a9e6cc0b892601662881
forresti <forresti@eecs.berkeley.edu>
2016-04-20 16:29:12 -0700

    don't use ex=1 by default

--- flops_wrapper.sh
+++ flops_wrapper.sh
@@ -4,5 +4,5 @@ source boda_env.sh
 #ptt_fn="%(models_dir)/%(in_model)/trainval.prototxt')" #ovreride from 'train_val' --> 'trainva;'
 boda cnet_ana --in-model=$net --print-ops=1 #--ptt-fn=$ptt_fn
 #python pysrc/flops.py --ai-mnk=1 --per-layer=1 --per-layer-in-info=1
-python pysrc/flops.py --print-dissertation-tex-table=1 --ex=1 
+python pysrc/flops.py --print-dissertation-tex-table=1 #--ex=1 
 
