8c1fb8d432f2f2f050e51c29d53311b1f16d3808
forresti <forresti@eecs.berkeley.edu>
2016-05-25 10:08:48 -0700

    arbitrary file path

--- flops_wrapper.sh
+++ flops_wrapper.sh
@@ -1,8 +1,12 @@
 #'net' is a directory that you choose within './nets'
 net=$1
 source boda_env.sh
-#ptt_fn="%(models_dir)/%(in_model)/trainval.prototxt')" #ovreride from 'train_val' --> 'trainva;'
-boda cnet_ana --in-model=$net --print-ops=1 #--ptt-fn=$ptt_fn
+
+#using default ptt_fn="%(models_dir)/%(in_model)/trainval.prototxt')"
+#boda cnet_ana --in-model=$net --print-ops=1
+
+#assuming 'net' is an absolute or relative path from `pwd`:
+boda cnet_ana --in-model=$net --print-ops=1 --ptt-fn='%(in_model)/train_val.prototxt'
 #python pysrc/flops.py --ai-mnk=1 --per-layer=1 --per-layer-in-info=1
 python pysrc/flops.py --print-dissertation-tex-table=1 #--ex=1 
 
